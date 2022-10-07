4e03e9e54739c7463ebc7a91817bae88f8171f5e
Shawn Smith <sds.smith24@gmail.com>
2022-10-07 09:58:56 -0400

    styling in home sidebars

--- src/components/page-components/open-source-sidebar/open-source-sidebar.component.tsx
+++ src/components/page-components/open-source-sidebar/open-source-sidebar.component.tsx
@@ -8,10 +8,13 @@ const OpenSourceSidebar: FC = () => {
     return (
         <div className={isMobile ? 'OpenSourceSidebarContainer OpenSourceSidebarMobile' : 'OpenSourceSidebarContainer'}>
             <h3>Open-source Contributions</h3>
-            <a className='ProjectButton' href='https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/spotify-api/index.d.ts' target='_blank' rel="noreferrer">
+            <div className='ProjectButton'>
+                <a href='https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/spotify-api/index.d.ts' target='_blank' rel="noreferrer">
                     <h4>DefinitelyTyped - Spotify API TypeScript Definitions</h4>
                     <p>contributed two new type definitions: RecommendationTrackObject & RecommendationAlbumObject</p>
-            </a>
+                </a>
+            </div>
+
         </div>
     )
 }
