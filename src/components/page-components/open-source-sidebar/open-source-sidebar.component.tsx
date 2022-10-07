c929c34c16bdddb7ad6889445e637d1a352c0c4f
Shawn Smith <sds.smith24@gmail.com>
2022-10-07 09:24:53 -0400

    styling in open source contributions

--- src/components/page-components/open-source-sidebar/open-source-sidebar.component.tsx
+++ src/components/page-components/open-source-sidebar/open-source-sidebar.component.tsx
@@ -9,10 +9,8 @@ const OpenSourceSidebar: FC = () => {
         <div className={isMobile ? 'OpenSourceSidebarContainer OpenSourceSidebarMobile' : 'OpenSourceSidebarContainer'}>
             <h3>Open-source Contributions</h3>
             <a className='ProjectButton' href='https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/spotify-api/index.d.ts' target='_blank' rel="noreferrer">
-                <div>
-                    <h2>DefinitelyTyped - Spotify API TypeScript Definitions</h2>
+                    <h4>DefinitelyTyped - Spotify API TypeScript Definitions</h4>
                     <p>contributed two new type definitions: RecommendationTrackObject & RecommendationAlbumObject</p>
-                </div>
             </a>
         </div>
     )
