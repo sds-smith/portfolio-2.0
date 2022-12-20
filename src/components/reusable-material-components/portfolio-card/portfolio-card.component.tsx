44c4eef3da206cf11c903e701bb1692b0d86c15d
Shawn Smith <sds.smith24@gmail.com>
2022-12-20 15:51:32 -0500

    update margins in portfolio card

--- src/components/reusable-material-components/portfolio-card/portfolio-card.component.tsx
+++ src/components/reusable-material-components/portfolio-card/portfolio-card.component.tsx
@@ -14,11 +14,11 @@ const PortfolioCard: FC<PortfolioCardProps> = ({ project, featured}) => {
             className='PortfolioCardContainer'
         >
             <img srcSet={project.background_image} alt={project.id} />
-            <h2>{project.heading_title}</h2>
-            <h3>{project.heading_subtitle}</h3>
+            <h2 className='textContent'>{project.heading_title}</h2>
+            <h3 className='textContent'>{project.heading_subtitle}</h3>
             {featured && 
                 <Fragment>
-                    <p>{project.description}</p>
+                    <p className='textContent'>{project.description}</p>
                     <div className='ButtonContainer'>
                          <LinkButton href={ project.live_link} >Open App</LinkButton>
                          <LinkButton href={ project.readme_link} >About</LinkButton>
